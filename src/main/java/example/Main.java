a7c23ff7fb84c99528399f1d54b6253e76805acf
max-ellis <mjellis@ualberta.ca>
2020-02-06 11:47:54 -0700

    initialize example

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,41 @@
+package example;
+
+
+public class Main {
+
+    public static void main(String args[]){
+      Parent1 parent = new Parent1();
+      Child1 child = new Child1();
+      int x = parent.foo();
+      int y = child.bar();
+    }
+
+}
+
+
+class Parent1 {
+
+  public int x = 5;
+  public int y = 10;
+  public int foo() {
+    where();
+    return x*y;
+  }
+
+  public void where() {
+    System.out.println("Here");
+  }
+
+}
+
+class Child1 extends Parent1 {
+
+  public int bar() {
+    where();
+    return x*y/2;
+  }
+}
+
+
+
+
